2e423268cf8a4060104b2be40b4073578bfc0be4
Unknown-Stryker <leehojin6580@gmail.com>
2025-10-21 16:31:20 +0900

    Add and edit assets

--- Launcher/Launcher/Assets/build.bat
+++ Launcher/Launcher/Assets/build.bat
@@ -6,7 +6,7 @@ cd /d "%SCRIPT_DIRECTORY%"
 mkdir Solution_X64_AVX
 cd Solution_X64_AVX
 
-cmake -DTARGET_CPU_ARCHITECTURE="x86-64" -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
+cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
 cmake --build . --config Debug -j8
 cmake --build . --config RelWithDebInfo -j8
 cmake --build . --config MinSizeRel -j8
@@ -19,7 +19,7 @@ cd /d "%SCRIPT_DIRECTORY%"
 mkdir Solution_X64_AVX512F
 cd Solution_X64_AVX512F
 
-cmake -DTARGET_CPU_ARCHITECTURE="x86-64" -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
+cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
 cmake --build . --config Debug -j8
 cmake --build . --config RelWithDebInfo -j8
 cmake --build . --config MinSizeRel -j8
