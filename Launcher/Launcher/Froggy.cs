5f6cd0b7eecb6ade40d4912e831b00119d588ae8
Unknown-Stryker <leehojin6580@gmail.com>
2025-11-12 20:49:28 +0900

    Update .froggy file structure

--- Launcher/Launcher/Froggy.cs
+++ Launcher/Launcher/Froggy.cs
@@ -17,6 +17,8 @@ public class EngineInfo
 [Serializable]
 public class GlobalResourceLookUpTable
 {
+    public string? EntryWorldPath { get; set; }
+
     public List<Tuple<string?, string?>> WorldPaths { get; set; } = new();
     public List<Tuple<string?, string?>> ModulePaths { get; set; } = new();
 }
@@ -27,26 +29,28 @@ public class ProjectInfo
     public string? ProjectType { get; set; }
     public string? ProjectName { get; set; }
     public string? ProjectPath { get; set; }
-
-    public string? EntryWorldPath { get; set; }
 }
 
 [Serializable]
 public class ProjectConfig
 {
+    public GlobalResourceLookUpTable GlobalResourceLookUpTable { get; set; } = new();
+
     public string? CompressionMethod { get; set; }
     public string? DecompressionMethod { get; set; }
 
     public string? EncryptionMethod { get; set; }
     public string? DecryptionMethod { get; set; }
 
-    public uint MaxEntities { get; set; } = 10240;
-    public uint MaxComponentTypeCountHint { get; set; } = 1024;
-    public uint MaxSystemCountHint { get; set; } = 1024;
+    public uint MaxEngineEntities { get; set; } = 1024;
+    public uint MaxEngineComponentTypeCountHint { get; set; } = 1024;
+    public uint MaxEngineSystemCountHint { get; set; } = 1024;
 
     public uint GCIterationsPerFrame { get; set; } = 30;
     public uint FiberStackSize { get; set; } = 1048576; // 1 MiB
     public uint FibersPerThread { get; set; } = 3;
+
+    public WindowConfig WindowConfig { get; set; } = new();
 }
 
 [Serializable]
@@ -78,9 +82,8 @@ public class Froggy // .froggy
 {
     public EngineInfo EngineInfo { get; set; } = new();
     public ProjectInfo ProjectInfo { get; set; } = new();
-    public GlobalResourceLookUpTable GlobalResourceLookUpTable { get; set; } = new();
+
     public ProjectConfig ProjectConfig { get; set; } = new();
-    public WindowConfig WindowConfig { get; set; } = new();
 }
 
 
