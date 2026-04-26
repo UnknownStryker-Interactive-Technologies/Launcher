9abb6a20058647a87b6202a57871097f55d01420
Unknown-Stryker <leehojin6580@gmail.com>
2026-04-26 15:37:36 +0900

    Update Froggy.cs

--- Launcher/Launcher/File Extension/Froggy.cs
+++ Launcher/Launcher/File Extension/Froggy.cs
@@ -4,26 +4,34 @@ namespace Launcher;
 
 
 [Serializable]
+public class WorldPath
+{
+    public string Name { get; set; } = String.Empty;
+    public string Path { get; set; } = String.Empty;
+}
+
+
+[Serializable]
 public class EngineInfo
 {
-    public string? Version { get; set; }
-    public string? InstallationPath { get; set; }
+    public string Version { get; set; } = String.Empty;
+    public string InstallationPath { get; set; } = String.Empty;
 }
 
 [Serializable]
 public class GlobalResourceLookUpTable
 {
-    public string? EntryWorldPath { get; set; }
+    public string EntryWorldPath { get; set; } = String.Empty;
 
-    public List<Tuple<string?, string?>> WorldPaths { get; set; } = new();
+    public List<WorldPath> WorldPaths { get; set; } = new();
 }
 
 [Serializable]
 public class ProjectInfo
 {
-    public string? ProjectType { get; set; }
-    public string? ProjectName { get; set; }
-    public string? ProjectPath { get; set; }
+    public string ProjectType { get; set; } = String.Empty;
+    public string ProjectName { get; set; } = String.Empty;
+    public string ProjectPath { get; set; } = String.Empty;
 }
 
 [Serializable]
@@ -31,11 +39,11 @@ public class ProjectConfig
 {
     public GlobalResourceLookUpTable GlobalResourceLookUpTable { get; set; } = new();
 
-    public string? CompressionMethod { get; set; }
-    public string? DecompressionMethod { get; set; }
+    public string CompressionMethod { get; set; } = String.Empty;
+    public string DecompressionMethod { get; set; } = String.Empty;
 
-    public string? EncryptionMethod { get; set; }
-    public string? DecryptionMethod { get; set; }
+    public string EncryptionMethod { get; set; } = String.Empty;
+    public string DecryptionMethod { get; set; } = String.Empty;
 
     public uint MaxEngineArchetypeCountHint { get; set; } = 1024;
     public uint MaxEngineComponentTypeCountHint { get; set; } = 1024;
@@ -51,13 +59,32 @@ public class ProjectConfig
 [Serializable]
 public class WindowConfig
 {
-    public string? Title { get; set; }
+    public string Title { get; set; } = String.Empty;
     public string[] IconPaths { get; set; } = { "Assets\\Icon\\runtime icon.png" };
     public string[] RandomPlayIntroVideoPaths { get; set; } = { "Assets\\Video\\runtime splash video 1.mp4", "Assets\\Video\\runtime splash video 2.mp4" };
     public string[] SequentialPlayIntroVideoPaths { get; set; } = Array.Empty<string>();
     public UInt16 SwapChainBufferCount { get; set; } = 3;
 }
-// -enable-fullscreen -enable-vsync
+
+
+
+
+[Serializable]
+public class ShaderDefines
+{
+    public string Identifier { get; set; } = String.Empty;
+    public string Value { get; set; } = String.Empty;
+}
+
+[Serializable]
+public class Shader
+{
+    public string Source { get; set; } = String.Empty;
+    public string MainFunction { get; set; } = String.Empty;
+    public string ShaderTarget { get; set; } = String.Empty;
+    public List<ShaderDefines> Defines { get; set; } = new();
+}
+
 
 
 
@@ -68,6 +95,7 @@ public class Froggy // .froggy
     public ProjectInfo ProjectInfo { get; set; } = new();
 
     public ProjectConfig ProjectConfig { get; set; } = new();
+    public List<Shader> Shaders { get; set; } = new();
 }
 
 
