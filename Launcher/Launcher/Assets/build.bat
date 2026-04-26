bb2332c4b79548492c85e334ebf4d6c7acd6f03a
Unknown-Stryker <leehojin6580@gmail.com>
2026-04-26 20:50:41 +0900

    Update build.bat

--- Launcher/Launcher/Assets/build.bat
+++ Launcher/Launcher/Assets/build.bat
@@ -7,10 +7,10 @@ mkdir Solution_X64_AVX
 cd Solution_X64_AVX
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143" 
-cmake --build . --config Debug -j8
-cmake --build . --config RelWithDebInfo -j8
-cmake --build . --config MinSizeRel -j8
-cmake --build . --config Release -j8
+cmake --build . --config Debug
+cmake --build . --config RelWithDebInfo
+cmake --build . --config MinSizeRel
+cmake --build . --config Release
 
 
 
@@ -20,9 +20,9 @@ mkdir Solution_X64_AVX512F
 cd Solution_X64_AVX512F
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
-cmake --build . --config Debug -j8
-cmake --build . --config RelWithDebInfo -j8
-cmake --build . --config MinSizeRel -j8
-cmake --build . --config Release -j8
+cmake --build . --config Debug
+cmake --build . --config RelWithDebInfo
+cmake --build . --config MinSizeRel
+cmake --build . --config Release
 
 endlocal
