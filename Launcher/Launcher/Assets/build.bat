2b69f3c2f495c31123b2602aa6d0d1b5bc622cea
Unknown-Stryker <leehojin6580@gmail.com>
2026-04-28 01:53:12 +0900

    Add CMake Build Option --parallel

--- Launcher/Launcher/Assets/build.bat
+++ Launcher/Launcher/Assets/build.bat
@@ -7,10 +7,10 @@ mkdir Solution_X64_AVX
 cd Solution_X64_AVX
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143" 
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
-cmake --build . --config MinSizeRel
-cmake --build . --config Release
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config MinSizeRel --parallel
+cmake --build . --config Release --parallel
 
 
 
@@ -20,9 +20,9 @@ mkdir Solution_X64_AVX512F
 cd Solution_X64_AVX512F
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G "Visual Studio 17 2022" -T "v143"
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
-cmake --build . --config MinSizeRel
-cmake --build . --config Release
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config MinSizeRel --parallel
+cmake --build . --config Release --parallel
 
 endlocal
