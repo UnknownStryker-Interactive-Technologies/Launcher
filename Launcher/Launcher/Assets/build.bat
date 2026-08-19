a7a071d617a1751f21a261122e596f49b726614f
Hojin Lee (Joey / Unknown Stryker) <leehojin6580@gmail.com>
2026-08-20 03:33:37 +0900

    Remove AVX512F build target from build script
    The asset build batch script no longer generates and builds the `Solution_X64_AVX512F` CMake configuration.

--- Launcher/Launcher/Assets/build.bat
+++ Launcher/Launcher/Assets/build.bat
@@ -13,16 +13,4 @@ cmake --build . --config MinSizeRel --parallel
 cmake --build . --config Release --parallel
 
 
-
-
-cd /d "%SCRIPT_DIRECTORY%"
-mkdir Solution_X64_AVX512F
-cd Solution_X64_AVX512F
-
-cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G %1 -T %2
-cmake --build . --config Debug --parallel
-cmake --build . --config RelWithDebInfo --parallel
-cmake --build . --config MinSizeRel --parallel
-cmake --build . --config Release --parallel
-
 endlocal
